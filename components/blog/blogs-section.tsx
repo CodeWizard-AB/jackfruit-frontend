5c65e1ce92cf22cf0b15962a5d9af7cbb14d0df0
Anunay <adobex092@gmail.com>
2025-10-25 13:31:31 +0600

    Fix typescript problem

--- components/blog/blogs-section.tsx
+++ components/blog/blogs-section.tsx
@@ -1,5 +1,6 @@
 import { clientContentful } from "@/lib/contentful";
 import BlogCard from "./blog-card";
+import { BlogEntry } from "@/types/contentful";
 
 export default async function BlogsSection() {
 	const { items } = await clientContentful.getEntries({
@@ -9,7 +10,7 @@ export default async function BlogsSection() {
 	return (
 		<section className="container">
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-				{items.map((item: { fields: any; sys: any }) => (
+				{items.map((item: { fields: BlogEntry; sys: any }) => (
 					<BlogCard key={item.sys.id} blog={item.fields} />
 				))}
 			</div>
